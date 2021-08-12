614fa9be8d9611b3c378fd48c92654ba870dad88
Vitalii <vitalikkalyta96@gmail.com>
2021-08-12 22:46:20 +0300

    Updated file Resolved_task_1.py.

--- HW_13_Decorators/Resolved_task_1.py
+++ HW_13_Decorators/Resolved_task_1.py
@@ -1,21 +1,7 @@
 # 1. Написати функцію яка в циклі зчитує з консолі введені користувачем дані і записує їх в список.
 # Написати декоратор який видасть час виконання роботи функції.
 #
-from time import sleep, time
-
-
-def user(*args):
-    def count(func):
-        def wrap(list_):
-            new_list = []
-            y = input("Enter number:")
-            for x in list_:
-                if isinstance(x, args):
-                    new_list.append(y)
-            result = func(new_list)
-            return result
-        return wrap
-    return count
+from time import time
 
 
 def time_check(func):
@@ -24,14 +10,16 @@ def time_check(func):
         result = func(*args, **kwargs)
         print(time() - t1)
         return result
+
     return wrap
 
 
-@user(int, float)
 @time_check
-def input_data(list_):
-    sleep(1)
-    return list_
+def input_data():
+    new_list = []
+    x = input("Enter number:")
+    new_list.append(x)
+    print(new_list)
 
 
-print(input_data([1]))
+input_data()
