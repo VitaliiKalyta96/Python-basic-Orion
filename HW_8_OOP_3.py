a88223db41f8b3f830095f81b616491b0a567e34
Vitalii <vitalikkalyta96@gmail.com>
2021-08-15 22:47:41 +0300

    Updated file with hw_8.

--- HW_8_OOP_3.py
+++ HW_8_OOP_3.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Dict, Any
-import random
+from random import randrange, choice
 import uuid
 import time
 
@@ -17,48 +17,63 @@ class Animal(ABC):
     def eat(self, forest: Forest):
         pass
 
-
-class Predator(Animal):
-
-    def __init__(self):
-        power = random.randrange(25, 100)
-        speed = random.randrange(25, 100)
-        Animal.__init__(self, power, speed)
-
-    def eat(self, forest: Forest):
-        victim: AnyAnimal = random.choice(list(forest.animals.values()))
-        if victim.id == self.id:
-            print(f"{self.__class__.__name__} unlucky, without dinner.")
-        else:
-            print(f"speed = {self.speed}, power = {self.current_power}, {self.__class__.__name__} "
-                  f"Run and Chase {victim.__class__.__name__}: power =  {victim.current_power}, speed = {victim.speed}")
-            if self.speed > victim.speed:
-                print(f"{self.__class__.__name__} caught {victim.__class__.__name__}")
-
-                if self.current_power < victim.current_power:
-                    print(f"{victim.__class__.__name__} animal survived.")
-                elif self.current_power > victim.current_power:
-                    print(f'{self.__class__.__name__} victim hunted.')
-
+    def hunt(self):
         self.current_power = round(self.current_power * 0.5, 1)
         if self.current_power <= 1:
             print(f'{self.__class__.__name__} died.')
             forest.remove_animal(self)
+        else:
+            print(f'{self.__class__.__name__} to reduce power to {self.current_power}')
+
+    def power_increase(self):
+        print(f'{self.__class__.__name__}: power = {self.current_power}')
+        self.current_power = round(self.current_power * 0.8, 1)
+        if self.current_power > self.max_power:
+            self.current_power = self.max_power
+            print(f'{self.__class__.__name__} restriction power to {self.max_power}')
+        else:
+            print(f'{self.__class__.__name__} will be recover power to {self.current_power}')
+
+
+class Predator(Animal):
+    def __init__(self):
+        power = randrange(25, 100)
+        speed = randrange(25, 100)
+        Animal.__init__(self, power, speed)
+
+    def eat(self, forest: Forest):
+        victim: AnyAnimal = choice(list(forest.animals.values()))
+
+        if victim.id == self.id:
+            print(f"{self.__class__.__name__} unlucky, without dinner.")
+        else:
+            print(f"{self.__class__.__name__}: speed = {self.speed}, power = {self.current_power}"
+                  f" Run and Chase {victim.__class__.__name__}: speed = {victim.speed}, power = {victim.current_power}")
+
+            if self.speed > victim.speed:
+                print(f"{self.__class__.__name__} caught {victim.__class__.__name__}")
+            else:
+                print(f'{self.__class__.__name__} did not caught up')
+                victim.hunt()
+                self.hunt()
+
+                if self.current_power < victim.current_power:
+                    print(f"{victim.__class__.__name__} animal survived.")
+                    self.hunt()
+                elif self.current_power > victim.current_power:
+                    print(f'{self.__class__.__name__} victim hunted.')
+                    self.power_increase()
 
 
 class Herbivorous(Animal):
 
     def __init__(self):
-        power = random.randrange(25, 100)
-        speed = random.randrange(25, 100)
+        power = randrange(25, 100)
+        speed = randrange(25, 100)
         Animal.__init__(self, power, speed)
 
     def eat(self, forest: Forest):
-        self.current_power += self.current_power * 0.7
-
-        if self.current_power > self.max_power:
-            self.current_power = self.max_power
-        print(f'Herbivorous has: {self.current_power} power.')
+        self.power_increase()
 
 
 AnyAnimal: Any[Herbivorous, Predator]
@@ -71,12 +86,14 @@ class Forest:
 
     def add_animal(self, animal: AnyAnimal):
         # self.animals.update({animal.id: animal})
+
         # The definition names/id animals
         self.animals.update({animal.id: animal})
-        print(f'The {animal.__class__.__name__} Added {animal.id}')
+        print(f'The {animal.__class__.__name__} added {animal.id}.')
 
     def remove_animal(self, animal: AnyAnimal):
-        self.animals.pop(animal.id)
+        if animal.id in self.animals.keys():
+            self.animals.pop(animal.id)
 
     def anybody_predators(self):
         animal_exist = False
@@ -88,7 +105,7 @@ class Forest:
 
 def animal_generator():
     while True:
-        generator_animal = random.choice([Herbivorous(), Predator()])
+        generator_animal = choice([Herbivorous(), Predator()])
         yield generator_animal
 
 
@@ -110,4 +127,3 @@ if __name__ == "__main__":
 
 print("Surviving in forest.")
 print("The end.")
-
