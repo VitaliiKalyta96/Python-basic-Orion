75663f2dd469e0374906d45f4335d3f8d38d07fa
Vitalii <vitalikkalyta96@gmail.com>
2021-08-18 23:41:15 +0300

    Added resolved hw_17_Practice.

--- HW_17_Practice/client.py
+++ HW_17_Practice/client.py
@@ -3,6 +3,10 @@ import threading
 import re
 
 
+class ExistUser(Exception):
+    pass
+
+
 def read_sock():
     while True:
         data = sock.recv(1024).decode('utf-8')
@@ -16,34 +20,45 @@ def read_sock():
             message = "@" + recipient + ", " + " you blocked by this user"
             sock.sendto(('[' + alias + '] ' + message).encode('utf-8'), server)
 
-
-server = ('46.4.63.238', 5050)
-alias = input("Your username: ")
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('', 0))
-sock.sendto(("[" + alias + "] Connect to server").encode('utf-8'), server)
-blocked_users = []
-
-potik = threading.Thread(target=read_sock)
-potik.start()
-
 while True:
-    print('1. Send message to group chat')
-    print('2. Send private message')
-    print('3. Block user')
+
+    server = ('192.168.31.188', 5151)
+    alias = input("Your username: ")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('', 0))
+    sock.sendto(("[" + alias + "] Connect to server").encode('utf-8'), server)
+    blocked_users = []
+    data_ = sock.recv(1024).decode('utf-8')
     try:
-        menu_choose = int(input(':'))
-    except ValueError:
+        if data_ == "User already exist! Input other name.":
+            print(data_)
+            raise ExistUser
+        else:
+            print("Congratulations! You are registered in system.")
+    except ExistUser:
+        print("Change user!")
         continue
-    if menu_choose == 1:
-        message = input("Your message: ")
-        sock.sendto(('[' + alias + '] ' + message).encode('utf-8'), server)
-    elif menu_choose == 2:
-        recipient = input('Recipient(Nickname): ')
-        message = input("Your message: ")
-        message = "@" + recipient + ", " + message
-        sock.sendto(('[' + alias + '] ' + message).encode('utf-8'), server)
-    elif menu_choose == 3:
-        recipient = input('Which user you want to block ? (Nickname): ')
-        blocked_users.append("[" + recipient + "]")
-        print("User " + recipient + " blocked!")
+
+    potik = threading.Thread(target=read_sock)
+    potik.start()
+
+    while True:
+        print('1. Send message to group chat')
+        print('2. Send private message')
+        print('3. Block user')
+        try:
+            menu_choose = int(input(':'))
+        except ValueError:
+            continue
+        if menu_choose == 1:
+            message = input("Your message: ")
+            sock.sendto(('[' + alias + '] ' + message).encode('utf-8'), server)
+        elif menu_choose == 2:
+            recipient = input('Recipient(Nickname): ')
+            message = input("Your message: ")
+            message = "@" + recipient + ", " + message
+            sock.sendto(('[' + alias + '] ' + message).encode('utf-8'), server)
+        elif menu_choose == 3:
+            recipient = input('Which user you want to block ? (Nickname): ')
+            blocked_users.append("[" + recipient + "]")
+            print("User " + recipient + " blocked!")
